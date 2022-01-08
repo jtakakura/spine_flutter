260d3c5317bfbdea7cca737cafd9dd2c617bbd57
Andriy Syrokomskyi <signmotion@gmail.com>
2022-01-08 11:52:23 +0200

    Enhanced the example and extended with raptor and its animation states.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,96 +6,120 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Flutter Spine Demo',
         theme: ThemeData(primarySwatch: Colors.blue),
-        home: const MyHomePage(title: 'Flutter + Spine'),
+        home: const MyHomePage(),
       );
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  SkeletonAnimation _skeleton;
+  String name;
+  String defaultAnimation;
+  List<String> animations;
+
+  SkeletonAnimation skeleton;
 
   @override
   void initState() {
     super.initState();
-    _loadSkeleton();
+
+    // spineboy
+    name = 'spineboy';
+    defaultAnimation = 'walk';
+    animations = <String>[
+      'death',
+      'idle',
+      'jump',
+      'hit',
+      'run',
+      'shoot',
+      'walk',
+    ];
+
+    // raptor
+    /*
+    name = 'raptor';
+    defaultAnimation = 'walk';
+    animations = <String>[
+      //'gun-grab',
+      //'gun-holster',
+      'jump',
+      'roar',
+      'walk',
+    ];
+    */
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-      backgroundColor: Colors.grey,
-      appBar: AppBar(title: Text(widget.title)),
-      body: Stack(children: <Widget>[
-        Positioned.fill(
-            child: SkeletonRenderObjectWidget(
-                skeleton: _skeleton,
-                alignment: Alignment.center,
-                fit: BoxFit.contain,
-                playState: PlayState.Playing)),
-        Positioned.fill(
-            child: Row(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-                margin: const EdgeInsets.all(5.0),
-                child: FlatButton(
-                    child: const Text('Jump'),
-                    textColor: Colors.white,
-                    color: Colors.blue,
-                    onPressed: () {
-                      _skeleton.state
-                        ..setAnimation(0, 'jump', false)
-                        ..addAnimation(0, 'walk', true, 0.0);
-                    })),
-            Container(
-                margin: const EdgeInsets.all(5.0),
-                child: FlatButton(
-                    child: const Text('Shoot'),
-                    textColor: Colors.white,
-                    color: Colors.blue,
-                    onPressed: () {
-                      setState(() {
-                        _skeleton.state
-                          ..setAnimation(0, 'shoot', false)
-                          ..addAnimation(0, 'walk', true, 0.0);
-                      });
-                    })),
-            Container(
-                margin: const EdgeInsets.all(5.0),
-                child: FlatButton(
-                    child: const Text('Death'),
-                    textColor: Colors.white,
-                    color: Colors.blue,
-                    onPressed: () {
-                      setState(() {
-                        _skeleton.state
-                          ..setAnimation(0, 'death', false)
-                          ..addAnimation(0, 'walk', true, 0.0);
-                      });
-                    })),
-          ],
-        ))
-      ]));
+  Widget build(BuildContext context) => FutureBuilder<SkeletonAnimation>(
+        future: loadSkeleton(),
+        builder:
+            (BuildContext context, AsyncSnapshot<SkeletonAnimation> snapshot) {
+          if (snapshot.hasData) {
+            skeleton = snapshot.data;
+            skeleton.state.setAnimation(0, defaultAnimation, true);
+            return _buildScreen();
+          }
 
-  void _loadSkeleton() {
-    SkeletonAnimation.createWithFiles(
-      'spineboy.atlas',
-      'spineboy.json',
-      'spineboy.png',
-      pathPrefix: 'assets/spineboy/',
-    ).then((SkeletonAnimation skeleton) {
-      skeleton.state.setAnimation(0, 'walk', true);
-      setState(() => _skeleton = skeleton);
-    });
+          return Container();
+        },
+      );
+
+  Widget _buildScreen() {
+    final SkeletonRenderObjectWidget skeletonWidget =
+        SkeletonRenderObjectWidget(
+      skeleton: skeleton,
+      alignment: Alignment.center,
+      fit: BoxFit.contain,
+      playState: PlayState.Playing,
+      debugRendering: false,
+      triangleRendering: true,
+    );
+
+    final List<Widget> buttons = <Widget>[];
+    for (final String animation in animations) {
+      buttons.add(
+        TextButton(
+          child: Text(animation.toUpperCase()),
+          onPressed: () {
+            skeleton.state
+              ..setAnimation(0, animation, false)
+              ..addAnimation(0, defaultAnimation, true, 0.0);
+          },
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(title: Text(name)),
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          skeletonWidget,
+          Positioned.fill(
+            child: Wrap(
+              runAlignment: WrapAlignment.end,
+              children: buttons,
+            ),
+          ),
+        ],
+      ),
+    );
   }
+
+  Future<SkeletonAnimation> loadSkeleton() async =>
+      await SkeletonAnimation.createWithFiles(
+        '$name.atlas',
+        '$name.json',
+        '$name.png',
+        pathPrefix: 'assets/$name/',
+      );
 }
